340006385a7221b8c7986b7a82391e59418c792e
mtleinon <mika.t.leinonen@gmail.com>
2019-07-01 21:25:37 +0300

    Added sorting to micronutrient report.

--- src/MealMicronutrientSummary.js
+++ src/MealMicronutrientSummary.js
@@ -3,6 +3,7 @@ import { MealContext } from './context/meal.context';
 import { NutritionContext } from './context/nutrition.context';
 import { ShowContext } from './context/show.context';
 import { MdClose } from 'react-icons/md';
+import { MdSort } from 'react-icons/md';
 import ReactResizeDetector from 'react-resize-detector';
 
 import Bar from './Bar';
@@ -21,6 +22,7 @@ export default function MealMicronutrientSummary({
   const nutritionContext = useContext(NutritionContext);
   const allNutritions = nutritionContext.nutritions;
   const nutritionInfo = nutritionContext.nutritionInfo;
+  const [sortBy, setSortBy] = useState('id');
 
   // Fill mealNutritions array either with a single nutrition, one meals nutritions all nutritions of all meals of a plan
   let mealNutritions = [];
@@ -83,18 +85,20 @@ export default function MealMicronutrientSummary({
 
     return recommendation ? (
       <>
-        <div className="reportNumber">
-          {recommendation >= 100
-            ? recommendation.toFixed(0)
-            : recommendation.toFixed(1)}
-        </div>
+        {wideComponent && (
+          <div className="reportNumber">
+            {recommendation >= 100
+              ? recommendation.toFixed(0)
+              : recommendation.toFixed(1)}
+          </div>
+        )}
         <div>
           <Bar value1={value} value2={recommendation} />
         </div>
       </>
     ) : (
       <>
-        <div className="reportNumber">-</div>
+        {wideComponent && <div className="reportNumber">-</div>}
         <div className="reportNumber">-</div>
       </>
     );
@@ -105,6 +109,12 @@ export default function MealMicronutrientSummary({
     if (a.name.fi.toLowerCase() > b.name.fi.toLowerCase()) return 1;
     return 0;
   };
+  const microNutrientComparisonById = (a, b) => {
+    if (a.id < b.id) return -1;
+    if (a.id > b.id) return 1;
+    return 0;
+  };
+
   const microNutritionComparisonByAmountOfRecommendation = (a, b) => {
     const aRecommendation = getRecommendation(a);
     const bRecommendation = getRecommendation(b);
@@ -123,6 +133,16 @@ export default function MealMicronutrientSummary({
     return aAmountOfRecommendation - bAmountOfRecommendation;
   };
 
+  const micronutrientComparison = (a, b) => {
+    switch (sortBy) {
+      case 'name':
+        return microNutritionComparisonByName(a, b);
+      case 'recommendation':
+        return microNutritionComparisonByAmountOfRecommendation(a, b);
+      default:
+        return microNutrientComparisonById(a, b);
+    }
+  };
   const onResize = width => {
     console.log(width);
     if (width < 500) {
@@ -137,7 +157,9 @@ export default function MealMicronutrientSummary({
       <ReactResizeDetector handleWidth onResize={onResize} />
       <div className="reportHeading">
         <div className="reportHeadingRow">
-          <div className="reportHeadingTitle">{name} Micronutrient Report</div>
+          <div className="reportHeadingTitle">
+            Micronutrient content of {name}
+          </div>
           <div className="reportHeadingIcons">
             <IconWithTooltip tooltipText="Close report">
               <MdClose className="icon" onClick={hide} />
@@ -149,19 +171,36 @@ export default function MealMicronutrientSummary({
             wideComponent ? 'wideReportGrid' : 'narrowReportGrid'
           }`}
         >
-          <div className="reportNumber">ID</div>
-          <div>name</div>
+          <div
+            className="reportNumber clickable"
+            onClick={() => setSortBy('id')}
+          >
+            ID <MdSort className="sortIcon" />
+          </div>
+          <div onClick={() => setSortBy('name')} className="clickable">
+            name <MdSort className="sortIcon" />
+          </div>
           {wideComponent ? (
             <>
               <div className="reportNumber">Amount</div>
-              <div className="reportNumber">Rec.</div>
-              <div className="reportNumber">% of rec.</div>
+              <div className="reportNumber">Rec</div>
+              <div
+                onClick={() => setSortBy('recommendation')}
+                className="reportNumber clickable"
+              >
+                % of Rec <MdSort className="sortIcon" />
+              </div>
               <div className="reportNumber">UL</div>
             </>
           ) : (
             <>
-              <div className="reportNumber">Rec.</div>
-              <div className="reportNumber">% of rec.</div>
+              <div className="reportNumber">Amount</div>
+              <div
+                onClick={() => setSortBy('recommendation')}
+                className="reportNumber clickable"
+              >
+                % of Rec <MdSort className="sortIcon" />
+              </div>
             </>
           )}
         </div>
@@ -173,18 +212,16 @@ export default function MealMicronutrientSummary({
       >
         {nutritionInfo
           .slice(2)
-          .sort(microNutritionComparisonByName)
+          .sort(micronutrientComparison)
           .map(microNutrition => (
             <React.Fragment key={microNutrition.id}>
               <div className="reportNumber">{microNutrition.id}.</div>
               <div>{microNutrition.name.fi}</div>
-              {wideComponent && (
-                <div className="reportNumber">
-                  {summary[microNutrition.id] >= 100
-                    ? summary[microNutrition.id].toFixed(0)
-                    : summary[microNutrition.id].toFixed(1)}
-                </div>
-              )}
+              <div className="reportNumber">
+                {summary[microNutrition.id] >= 100
+                  ? summary[microNutrition.id].toFixed(0)
+                  : summary[microNutrition.id].toFixed(1)}
+              </div>
               {recommendationAndValue(
                 microNutrition,
                 summary[microNutrition.id]
